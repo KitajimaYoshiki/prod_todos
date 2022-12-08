3e1bacc73b0d0cc4e5d16a9ed26b035e04738f6b
yoshiki kitajima <yoshiki.kitajima@jp.ricoh.com>
2022-12-08 13:39:44 +0900

    画面概形

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,44 @@
-import AppBar from "@pages/api/AppBar";
+import AppBar from "./parts/AppBar";
 import * as React from 'react';
 import LoadingButton from '@mui/lab/LoadingButton';
 import Box from '@mui/material/Box';
 import { useState } from "react";
-import LoadingButtons from "./api/LoadingButtons";
+import LoadingButtons from "./parts/LoadingButtons";
+import { bgcolor, Container } from "@mui/system";
+import SelectBox from "./parts/SelectBox";
+import Tables from "./parts/Tables";
 
 export default function Home() {
 
   return (
     <div>
       <AppBar></AppBar>
+      <Container maxWidth='xl'>
       <Box 
-        sx={{ 
-          display: 'grid',
-          gridTemplateColumns: 'repeat(3, 1fr)',
-          margin: 3,
-          justifyContent: 'center',
-          alignItems: 'center',
-          gridTemplateRows: 'auto',
-          mx: 'auto',
-          width: 200
+        sx={{
+          // bgcolor: 'red',
+          height: '100vh'
         }}
       >
-        <LoadingButtons time={1000}/>
-        <LoadingButtons time={500}/>
-        <LoadingButtons />
+        <Box 
+          sx={{ 
+            display: 'grid',
+            gridTemplateColumns: 'repeat(2, 5fr)',
+            margin: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+            gridTemplateRows: 'auto',
+            mx: 'auto',
+            width: 500,
+            // bgcolor: 'skyblue',
+          }}
+        >
+          <LoadingButtons time={1000}/>
+          <SelectBox></SelectBox>
+        </Box>
+        <Tables />
       </Box>
+      </Container>
     </div>
   )
 }
---
+++ pages/parts/SelectBox.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import FormHelperText from '@mui/material/FormHelperText';
+
+export default function BasicSelect() {
+  const [tags, setTags] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setTags(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 2, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Tag</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={tags}
+          label="Tag"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>なし</em>
+          </MenuItem>
+          <MenuItem value={1}>重要</MenuItem>
+          <MenuItem value={2}>優先</MenuItem>
+          <MenuItem value={3}>標準</MenuItem>
+          <MenuItem value={11}>受講</MenuItem>
+          <MenuItem value={12}>郵送</MenuItem>
+          <MenuItem value={13}>業務</MenuItem>
+        </Select>
+        <FormHelperText>表示するタスクのタグを選択</FormHelperText>
+      </FormControl>
+    </div>
+  );
+}
