6ae4d3868a04fd3047ad725203e25a0179477ee8
yoshiki kitajima <yoshiki.kitajima@jp.ricoh.com>
2022-12-14 15:54:07 +0900

    仮実装

--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+  },
+  // ...
+}
